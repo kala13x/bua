674185a5a94eb85491dc06cb26a293a0f9f501de
Sandro Kalatozishvili <skalatozishvili@cloudlinux.com>
2024-05-24 03:04:48 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## Installation Instructions
+
+Follow these steps to install the extension in development mode:
+
+1. **Download the Extension Files**
+   - Clone this repository or download the ZIP file and extract it to a directory on your computer.
+
+2. **Open Chrome Extensions Page**
+   - Open Google Chrome.
+   - Go to `chrome://extensions/`.
+
+3. **Enable Developer Mode**
+   - Toggle the switch in the top right corner to enable "Developer mode".
+
+4. **Load Unpacked Extension**
+   - Click on the "Load unpacked" button.
+   - Select the directory where you extracted the extension files.
+
+5. **Verify Installation**
+   - The extension should now appear in your list of installed extensions.
+
+## Example
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/kala13x/bua/main/example.png" alt="Example Screenshot" width="600">
+</p>
---
+++ extension/content.js
@@ -0,0 +1,26 @@
+const replacements = [
+    { search: "გლობალური ომის პარტია", replace: "ბუა" },
+    { search: "გლობალურმა ომის პარტიამ", replace: "ბუამ" },
+    { search: "გლობალურ ომის პარტიას", replace: "ბუას" },
+    { search: "გლობალური ომის პარტიის", replace: "ბუის" },
+    { search: "გლობალური ომის პარტიით", replace: "ბუით" },
+    { search: "გლობალურ ომის პარტიად", replace: "ბუად" },
+    { search: "გლობალურო ომის პარტიავ", replace: "ბუავ" },
+    { search: "გლობალურ ომის პარტიაზე", replace: "ბუაზე" }
+  ];
+
+  function replaceTextOnPage(replacements) {
+    const textNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+    let currentNode;
+  
+    while ((currentNode = textNodes.nextNode())) {
+      replacements.forEach(({ search, replace }) => {
+        if (currentNode.nodeValue.includes(search)) {
+          currentNode.nodeValue = currentNode.nodeValue.replace(new RegExp(search, 'g'), replace);
+        }
+      });
+    }
+  }
+
+  // Execute the replacement function
+  replaceTextOnPage(replacements);
